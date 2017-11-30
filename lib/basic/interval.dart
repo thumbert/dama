ceab73adf3e8e603514cabb6c32cb76439662125
thumbert <humbert.tony@yahoo.com>
2017-11-29 22:29:44 -0500

    added more work on interval

--- lib/basic/interval.dart
+++ lib/basic/interval.dart
@@ -1,5 +1,7 @@
 library basic.interval;
 
+import 'package:func/func.dart';
+
 enum IntervalType {
   closedOpen,
   openClosed,
@@ -37,16 +39,32 @@ class Interval<K extends Comparable<K>> {
     return res;
   }
 
+
   /// Split this interval into two intervals: a left interval [start,value)
   /// and a right interval [value, end).  The [intervalType] is kept the
   /// same.
-  List<Interval> split(K value) {
-    if (start.compareTo(value) >= 0 || value.compareTo(end) >= 0)
-      throw 'Split value needs to be inside the interval.';
-    Interval left = new Interval(start, value, intervalType: intervalType);
-    Interval right = new Interval(value, end, intervalType: intervalType);
-    return [left, right];
+//  List<Interval> split(K value) {
+//    if (start.compareTo(value) >= 0 || value.compareTo(end) >= 0)
+//      throw 'Split value needs to be inside the interval.';
+//    Interval left = new Interval(start, value, intervalType: intervalType);
+//    Interval right = new Interval(value, end, intervalType: intervalType);
+//    return [left, right];
+//  }
+
+  /// Starting from the left value, generate an iterable of intervals according
+  /// to the function f.
+  /// <p>For example to split the interval [0,10) into 10 intervals of length 1
+  /// each, use the function f = (x) => new Interval(x, x+1)
+  List<Interval> splitLeft(Func1<K,Interval> f) {
+    List res = [];
+    Interval current = f(start);
+    while (current.end.compareTo(end) < 1) {
+      res.add(current);
+      current = f(current.end);
+    }
+    return res;
   }
+
 
   String toString() {
     String res;
--- test/basic/interval_test.dart
+++ test/basic/interval_test.dart
@@ -6,7 +6,29 @@ import 'package:dama/basic/interval.dart';
 intervalTest() {
   test('Create interval [0,1)', () {
     Interval i = new Interval(0, 1);
-    print(i);
+    expect(i.start, 0);
+    expect(i.end, 1);
+    expect(i.toString(), '[0,1)');
+  });
+
+  test('Create interval [0,\infty)', () {
+    Interval i = new Interval(0.0, double.INFINITY);
+    expect(i.start, 0.0);
+    expect(i.end, double.INFINITY);
+    expect(i.toString(), '[0.0,Infinity)');
+  });
+
+
+  test('Split interval [0,10) into 10 unit intervals', () {
+    Interval ival = new Interval(0, 10);
+    List ivals = ival.splitLeft((x) => new Interval(x, x+1));
+    expect(ivals.length, 10);
+  });
+
+  test('Split interval [new DateTime(2017),new DateTime(2018)) into hours', () {
+    Interval year = new Interval(new DateTime(2017), new DateTime(2018));
+    List hours = year.splitLeft((x) => new Interval(x, x.add(new Duration(hours: 1))));
+    expect(hours.length, 8760);
   });
 
 
