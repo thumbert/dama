aa9dce995338c96bca01aab3cfd4940ab3ca391f
thumberts <tony.humberts@gmail.com>
2019-06-24 11:26:31 -0400

    linear filters

--- lib/basic/linear_filter.dart
+++ lib/basic/linear_filter.dart
@@ -3,38 +3,28 @@ library basic.linear_filter;
 import 'dart:math' show pow;
 import 'package:more/math.dart';
 
-enum Align {past, center, future}
+enum Align { past, center, future }
 
-List<num> movingAverageFilter(List<num> x, List<num> coefficients,
-    Align align) {
-  return [0];
-}
 
-/// Filter data according to a binomial filter of a given order.
-/// When data can't be calculated, return null.
-/// The order needs to be an even integer.
-///
-List<num> binomialFilter(List<num> x, int order, {Align align: Align.center}) {
-  if (order <= 1) throw ArgumentError('Order should be > 1');
-  if (order % 2 != 0) throw ArgumentError('The order needs to be even');
-  if (2*order+1 > x.length)
-    throw ArgumentError('Input list has too few elements');
+/// Calculate a moving average.
+/// <p>Argument [weights] a numeric list of length n.
+/// <p>Align past uses the last n available observations.
+List<num> movingAverageFilter(
+    List<num> x, List<num> weights, {Align align: Align.past}) {
 
-  var weights = _getBinomialWeights(order);
   int offset;
-
-  var out = List<num>(x.length);
-  if (align == Align.center) {
-    offset = order ~/ 2;
-  } else if (align == Align.past) {
-    offset = 0;
+  if (align == Align.past) {
+    offset = weights.length - 1;
   } else {
     throw ArgumentError('$align not implemented yet');
   }
+
+  var out = List<num>(x.length);
   num value;
-  for (int i=offset; i<x.length-offset; i++) {
+  // the bulk
+  for (int i = offset; i < x.length; i++) {
     value = 0.0;
-    for (int j=0; j<weights.length; j++) {
+    for (int j = 0; j < weights.length; j++) {
       value += weights[j] * x[i + j - offset];
     }
     out[i] = value;
@@ -43,13 +33,70 @@ List<num> binomialFilter(List<num> x, int order, {Align align: Align.center}) {
   return out;
 }
 
+/// Filter data according to a binomial filter of a given order.
+/// When data can't be calculated, return null.
+/// <p>Argument [order] needs to be an even integer.
+/// <p>If argument [circular] set to [true] the calculation at both ends wraps
+/// around the ends of the series, so no nulls are returned.  This is useful
+/// if the data is truly periodic.
+///
+List<num> binomialFilter(List<num> x, int order, {bool circular: false}) {
+  if (order <= 1) throw ArgumentError('Order should be > 1');
+  if (order % 2 != 0) throw ArgumentError('The order needs to be even');
+  if (2 * order + 1 > x.length)
+    throw ArgumentError('Input list has too few elements');
 
+  var weights = _getBinomialWeights(order);
+  int offset = order ~/ 2;
+
+  var out = List<num>(x.length);
+  num value;
+  if (circular) {
+    // the beginning
+    for (int i = 0; i < offset; i++) {
+      value = 0.0;
+      for (int j = 0; j < weights.length; j++) {
+        int k = i + j - offset;
+        if (k < 0) k += x.length;
+        value += weights[j] * x[k];
+      }
+      out[i] = value;
+    }
+  }
+
+  // the bulk
+  for (int i = offset; i < x.length - offset; i++) {
+    value = 0.0;
+    for (int j = 0; j < weights.length; j++) {
+      value += weights[j] * x[i + j - offset];
+    }
+    out[i] = value;
+  }
+
+  if (circular) {
+    // the end
+    for (int i = x.length - offset; i < x.length; i++) {
+      value = 0.0;
+      for (int j = 0; j < weights.length; j++) {
+        int k = i + j - offset;
+        if (k >= x.length) k -= x.length;
+        value += weights[j] * x[k];
+      }
+      out[i] = value;
+    }
+  }
+
+
+  return out;
+}
+
+/// For n=2, return [1,2,1]; for n=4, return [1,4,6,4,1], etc.
 List<double> _getBinomialWeights(int n) {
   var denominator = pow(2, n);
-  var out = List<double>(n+1);
+  var out = List<double>(n + 1);
   for (int k = 0; k < n; k++) {
-    out[k] = binomial(n,k)/denominator;
-    out[n-k] = out[k];
+    out[k] = binomial(n, k) / denominator;
+    out[n - k] = out[k];
   }
   return out;
 }
--- test/basic/linear_filter_test.dart
+++ test/basic/linear_filter_test.dart
@@ -5,20 +5,39 @@ import 'package:test/test.dart';
 
 
 tests() {
-  group('Linear filter tests', () {
+  group('Linear filter tests:', () {
     test('Binomial filter, order 2', () {
       var x = <num>[12, 17, 10, 22, 15, 11, 18, 27, 14];
-      var y = binomialFilter(x, 2, align: Align.center);
+      var y = binomialFilter(x, 2);
       var yExp = [null, 14, 14.75, 17.25, 15.75, 13.75, 18.50, 21.50, null];
       expect(yExp, y);
     });
     test('Binomial filter, order 4', () {
       var x = <num>[12, 17, 10, 22, 15, 11, 18, 27, 14];
-      var y = binomialFilter(x, 4, align: Align.center);
+      var y = binomialFilter(x, 4);
       expect(y[0], null);
       expect(y[1], null);
       expect(y[2], 15.1875);
     });
+    test('Binomial filter, order 4, circular: true', () {
+      var x = <num>[12, 17, 10, 22, 15, 11, 18, 27, 14];
+      var y = binomialFilter(x, 4, circular: true);
+      var y0Exp = (27 + 14*4 + 12*6 + 17*4 + 10)/16;
+      var y1Exp = (14 + 12*4 + 17*6 + 10*4 + 22)/16;
+      var y8Exp = (18 + 27*4 + 14*6 + 12*4 + 17)/16;
+      expect(y[0], y0Exp);
+      expect(y[1], y1Exp);
+      expect(y[8], y8Exp);
+      expect(y[2], 15.1875);
+    });
+    test('Moving average filter', () {
+      var x = <num>[12, 17, 10, 22, 15, 11, 18, 27, 14];
+      var weights = <num>[0.25, 0.25, 0.25, 0.25];
+      var y = movingAverageFilter(x, weights);
+      var yExp = [null, null, null, 15.25, 16.0, 14.5, 16.5, 17.75, 17.5];
+      expect(yExp, y);
+    });
+
   });
 
 }
