549a321c06004b4175de6a61395d687c311ba483
thumbert <humbert.tony@yahoo.com>
2020-04-29 21:44:23 -0400

    add QuantilePair convenience class

--- lib/stat/descriptive/quantile.dart
+++ lib/stat/descriptive/quantile.dart
@@ -2,6 +2,8 @@ library stat.descriptive.quantile;
 
 import 'dart:collection';
 
+import 'package:more/hash.dart';
+
 /// See http://en.wikipedia.org/wiki/Quantile
 enum QuantileEstimationType { R1, R2, R3, R4, R5, R6, R7, R8, R9 }
 
@@ -9,6 +11,27 @@ class OutOfRangeException implements Exception {
   String message;
   OutOfRangeException([message]);
 }
+
+class QuantilePair {
+  final num probability;
+  final num value;
+
+  /// For readability, a class to keep track of the mapping probability -> value
+  QuantilePair(this.probability, this.value) {
+    if (probability < 0 || probability > 1) {
+      throw OutOfRangeException('Invalid probability $probability value');
+    }
+  }
+  @override
+  int get hashCode => hash2(probability, value);
+  @override
+  bool operator ==(other) {
+    if (other is! QuantilePair) return false;
+    QuantilePair qp = other;
+    return qp.probability == probability && qp.value == value;
+  }
+}
+
 
 ///No need to do a full sorting Nlog(N).  Partition based selection is a
 ///linear-time algorithm.  Caching the pivot, speeds up the search even more
@@ -30,29 +53,29 @@ class Quantile {
   ///
   /// See http://en.wikipedia.org/wiki/Quantile for different quantile estimation methods.
   Quantile(List<num> x,
-      {QuantileEstimationType this.quantileEstimationType:
-          QuantileEstimationType.R7,
+      {this.quantileEstimationType = QuantileEstimationType.R7,
       bool shuffle: true}) {
-    _x = new List.from(x, growable: false);
+    _x = List.from(x, growable: false);
     if (shuffle) _x.shuffle();
-    _cachedK = new SplayTreeSet();
+    _cachedK = SplayTreeSet();
   }
 
   /// Calculate the quantile corresponding to the given probability level
   /// [probability].
   num value(num probability) {
     //print('computing probability $probability');
-    if (probability < 0 || probability > 1)
-      throw new OutOfRangeException('probability has to be between [0,1]');
-    if (_x.length == 0) return double.nan;
+    if (probability < 0 || probability > 1) {
+      throw OutOfRangeException('probability has to be between [0,1]');
+    }
+    if (_x.isEmpty) return double.nan;
     if (_x.length == 1) return _x[0];
     if (probability == 1) return minK(_x.length - 1);
 
     num result;
     switch (quantileEstimationType) {
       case QuantileEstimationType.R1:
-        num h = _indexR1(probability, _x.length);
-        int hf = (h - 0.5).ceil();
+        var h = _indexR1(probability, _x.length);
+        var hf = (h - 0.5).ceil();
         minK(hf);
         result = _x[hf];
         break;
@@ -60,8 +83,8 @@ class Quantile {
       case QuantileEstimationType.R4:
 
         /// linear interpolation of the empirical cdf
-        num h = _indexR4(probability, _x.length);
-        int hf = h.floor();
+        var h = _indexR4(probability, _x.length);
+        var hf = h.floor();
         minK(hf);
         minK(hf + 1);
         result = _x[hf] + (h - hf) * (_x[hf + 1] - _x[hf]);
@@ -70,8 +93,8 @@ class Quantile {
       case QuantileEstimationType.R7:
 
         /// R's default computation
-        num h = _indexR7(probability, _x.length);
-        int hf = h.floor();
+        var h = _indexR7(probability, _x.length);
+        var hf = h.floor();
         minK(hf);
         minK(hf + 1);
         result = _x[hf] + (h - hf) * (_x[hf + 1] - _x[hf]);
@@ -85,52 +108,52 @@ class Quantile {
   }
 
   /// Calculate the median value.
-  median() => value(0.5);
+  num median() => value(0.5);
 
   /// Return the k-th smallest element of the array.  The input argument [k] is
   /// between 0 and [_x.length]-1.  So if k=0, you return the array minimum.
   /// Uses a cache to get the lowest k value if it has already been calculated.
-  minK(int k) {
-    if (_cachedK.contains(k))
+  num minK(int k) {
+    if (_cachedK.contains(k)) {
       return _x[k];
-    else {
+    } else {
       _cachedK.add(k);
       return _getLowestK(k);
     }
   }
 
-  /**
-   *  To speed up the search, you don't need to scan the entire list.
-   *  Only between two k's that are not in the cache.
-   */
-  _getLowestK(int k) {
+  ///  To speed up the search, you don't need to scan the entire list.
+  ///  Only between two k's that are not in the cache.
+  num _getLowestK(int k) {
     assert(k <= _x.length - 1);
-    int lo = _cachedK.lastWhere((e) => e < k, orElse: () => 0);
-    int hi = _cachedK.firstWhere((e) => e > k, orElse: () => _x.length - 1);
+    var lo = _cachedK.lastWhere((e) => e < k, orElse: () => 0);
+    var hi = _cachedK.firstWhere((e) => e > k, orElse: () => _x.length - 1);
     while (hi > lo) {
-      int j = _partition(lo, hi, lo);
-      if (j == k)
-        return _x[k]; // done
-      else if (j > k)
+      var j = _partition(lo, hi, lo);
+      if (j == k) {
+        return _x[k];
+      } else if (j > k) {
         hi = j - 1;
-      else if (j < k) lo = j + 1;
+      } else if (j < k) lo = j + 1;
     }
 
     return _x[k];
   }
 
-  /**
-   * Rearrange the input vector x[lo] to x[hi] and return an integer j (pivot) such that
-   * x[lo] to x[j-1] are less than x[j] and x[j+1] to x[hi] are higher than x[j].
-   * The pivot is the initial index of the pivot element.
-   *
-   */
+  /// Rearrange the input vector x[lo] to x[hi] and return an integer j (pivot) such that
+  /// x[lo] to x[j-1] are less than x[j] and x[j+1] to x[hi] are higher than x[j].
+  /// The pivot is the initial index of the pivot element.
+  ///
   int _partition(int lo, int hi, int pivot) {
-    int i = lo, j = hi + 1;
+    var i = lo, j = hi + 1;
     var v = _x[pivot];
     while (true) {
-      while (_x[++i].compareTo(v) < 0) if (i == hi) break;
-      while (v.compareTo(_x[--j]) < 0) if (j == lo) break;
+      while (_x[++i].compareTo(v) < 0) {
+        if (i == hi) break;
+      }
+      while (v.compareTo(_x[--j]) < 0) {
+        if (j == lo) break;
+      }
       if (i >= j) break;
       _swap(i, j);
     }
@@ -146,25 +169,27 @@ class Quantile {
 }
 
 num _indexR1(num probability, int length) {
-  if (probability == 0)
+  if (probability == 0) {
     return 0;
-  else
-    return probability * length -
-        0.5; // wikipedia formulas use 1 based indexing!
+  } else {
+    return probability * length - 0.5;
+  } // wikipedia formulas use 1 based indexing!
 }
 
 num _indexR4(num probability, int length) {
-  if (probability < 1.0 / length)
+  if (probability < 1.0 / length) {
     return 0.0;
-  else if (probability == 1.0)
+  } else if (probability == 1.0) {
     return length - 1;
-  else
+  } else {
     return probability * length - 1;
+  }
 }
 
 num _indexR7(num probability, int length) {
-  if (probability == 1.0)
+  if (probability == 1.0) {
     return length - 1;
-  else
+  } else {
     return probability * (length - 1);
+  }
 }
--- test/stat/descriptive/quantile_test.dart
+++ test/stat/descriptive/quantile_test.dart
@@ -4,57 +4,69 @@ import 'dart:math';
 import 'package:test/test.dart';
 import 'package:dama/stat/descriptive/quantile.dart';
 
-testQuantile() {
+void tests() {
+  group('QuantilePair test:', () {
+    test('basic', (){
+      var qp1 = QuantilePair(0.3, 5.1);
+      var qp2 = QuantilePair(0.3, 5.1);
+      expect(qp1, qp2);
+      expect({qp1, qp2}, {qp1});
+      var m1 = {qp1: 42};
+      expect(m1[qp2], 42);
+    });
+
+  });
+
   group('Quantile test:', () {
     test('minK', () {
       var x = [3, 1, 5, 9, 4, 2, 7];
-      Quantile q = new Quantile(x);
-      var res = new List.generate(7, (i) => i).map((e) => q.minK(e));
+      var q = Quantile(x);
+      var res = List.generate(7, (i) => i).map((e) => q.minK(e));
       expect(res, [1, 2, 3, 4, 5, 7, 9]);
     });
 
     test('R1 quantile calculation, sorted array', () {
-      var x = new List.generate(8, (i) => i);
-      Quantile q = new Quantile(x,
+      var x = List.generate(8, (i) => i);
+      var q = Quantile(x,
           shuffle: false, quantileEstimationType: QuantileEstimationType.R1);
-      List probs = [0, 0.25, 0.5, 0.75, 1];
+      var probs = [0, 0.25, 0.5, 0.75, 1];
       var res = probs.map((p) => q.value(p)).toList();
       expect(res, [0, 1, 3, 5, 7]);
     });
 
     test('R4 quantile calculation, sorted array', () {
-      var x = new List.generate(3, (i) => i);
-      Quantile q = new Quantile(x,
+      var x = List.generate(3, (i) => i);
+      var q = Quantile(x,
           shuffle: false, quantileEstimationType: QuantileEstimationType.R4);
-      List probs = [0, 0.25, 0.5, 0.75, 1];
+      var probs = [0, 0.25, 0.5, 0.75, 1];
       var res = probs.map((p) => q.value(p)).toList();
       expect(res, [0, 0, 0.5, 1.25, 2]);
     });
 
     test('R7 quantile calculation, sorted array', () {
-      var x = new List.generate(8, (i) => i);
-      Quantile q = new Quantile(x,
+      var x = List.generate(8, (i) => i);
+      var q = Quantile(x,
           shuffle: false, quantileEstimationType: QuantileEstimationType.R7);
-      List probs = [0, 0.25, 0.5, 0.75, 1];
+      var probs = [0, 0.25, 0.5, 0.75, 1];
       var res = probs.map((p) => q.value(p)).toList();
       expect(res, [0, 1.75, 3.5, 5.25, 7]);
     });
 
     test('R7 quantile calculation, unsorted array', () {
-      var x = new List.generate(8, (i) => i);
+      var x = List.generate(8, (i) => i);
       //x.shuffle();
-      Quantile q = new Quantile(x,
+      var q = Quantile(x,
           shuffle: true, quantileEstimationType: QuantileEstimationType.R7);
-      List probs = [0, 0.25, 0.5, 0.75, 1];
+      var probs = [0, 0.25, 0.5, 0.75, 1];
       var res = probs.map((p) => q.value(p)).toList();
       expect(res, [0, 1.75, 3.5, 5.25, 7]);
     });
 
     test('default quantiles, unsorted array', () {
-      var x = new List.generate(10, (i) => i+1);
-      x.shuffle(new Random(0));
-      Quantile q = new Quantile(x, shuffle: false);
-      List probs = [0, 0.25, 0.5, 0.75, 1];
+      var x = List.generate(10, (i) => i+1);
+      x.shuffle(Random(0));
+      var q = Quantile(x, shuffle: false);
+      var probs = [0, 0.25, 0.5, 0.75, 1];
       var res = probs.map((p) => q.value(p)).toList();
       expect(res, [1, 3.25, 5.5, 7.75, 10]);
     });
@@ -65,19 +77,19 @@ speedTest() {
   //How it is so fast?!  takes 11 milliseconds to do this!
   test('performance for large inputs', () {
     int N = 20000000;
-    List x = new List.generate(N, (i) => i);
-    x.shuffle(new Random(0));
-    Quantile q = new Quantile(x, shuffle: false);
-    var w = new Stopwatch()..start();
-    var res = new List.generate(1000000, (i) => i * 200).map((e) => q.minK(e));
+    List x = List.generate(N, (i) => i);
+    x.shuffle(Random(0));
+    Quantile q = Quantile(x, shuffle: false);
+    var w = Stopwatch()..start();
+    var res = List.generate(1000000, (i) => i * 200).map((e) => q.minK(e));
     print('evaluation microseconds: ${w.elapsedMicroseconds}');
     w.stop();
     print(res);
   });
 }
 
-main() {
-  testQuantile();
+void main() {
+  tests();
 }
 
 // NAN's are put after the max value
