8946e185a9ea8bfad4d456381c97cad8a212db97
thumbert <humbert.tony@yahoo.com>
2020-12-30 08:27:12 -0500

    do it for double too

--- lib/basic/num_extensions.dart
+++ lib/basic/num_extensions.dart
@@ -11,3 +11,13 @@ extension NumExtensions on num {
         max(relativeTolerance * max(abs(), x.abs()), absoluteTolerance);
   }
 }
+
+extension DoubleExtensions on double {
+  /// Compare if two numbers are close up to a relative tolerance.
+  /// Copied from https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python
+  bool isCloseTo(double x,
+      {num relativeTolerance = 1e-9, num absoluteTolerance = 0.0}) {
+    return (this - x).abs() <=
+        max(relativeTolerance * max(abs(), x.abs()), absoluteTolerance);
+  }
+}
